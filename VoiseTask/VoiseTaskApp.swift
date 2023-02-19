975020da0df2db47605b0ac07f3884e35ef6aaf0
Mustafa Magdy <mustafa.magdy@linkdev.com>
2023-02-19 20:38:55 +0200

    Initial Commit

---
+++ VoiseTask/VoiseTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiseTaskApp.swift
+//  VoiseTask
+//
+//  Created by Mustafa Magdy on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct VoiseTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
